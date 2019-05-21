6572c6a0ec0e1306fb92e21ad0311eaf760bb1a4
Marco Cordeiro <37083749+cordeiromarco@users.noreply.github.com>
2019-05-21 19:14:57 -0300

    Update connections.tf
    Re

--- connections.tf
+++ connections.tf
@@ -3,8 +3,11 @@ provider "aws" {
 }
 
 provider "azurerm" {
-  subscription_id = "7f0d3f72-d6ff-488f-9ac7-02ad42ff1074"
+  subscription_id = "-48
+
+
+4"
   client_id = "1"
   client_secret = "2"
-  tenant_id = "b96b97e6-5234-47a3-9869-2345c639abd9"
+  tenant_id = "b-99"
 }
