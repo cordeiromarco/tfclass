b6ec998b4cedba55f57a1bba1919eda353b13f08
Marco Cordeiro <cordeirom@gmail.com>
2019-05-21 16:07:34 -0300

    first deploy

---
+++ connections.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "azurerm" {
+  subscription_id = "7f0d3f72-d6ff-488f-9ac7-02ad42ff1074"
+  client_id = "1"
+  client_secret = "2"
+  tenant_id = "b96b97e6-5234-47a3-9869-2345c639abd9"
+}
